d57ff2e21257f2744c29f1fda364b18c5cbdb2c0
DaveSomo <dhsomo@gmail.com>
2015-03-23 14:51:28 -0400

    Updated Checker and IdentificationTable

--- src/miniJava/ContextualAnalyzer/Checker.java
+++ src/miniJava/ContextualAnalyzer/Checker.java
@@ -1,6 +1,206 @@
 package miniJava.ContextualAnalyzer;
 
-public class Checker {
+import miniJava.ErrorReporter;
+import miniJava.AbstractSyntaxTrees.*;
+import miniJava.AbstractSyntaxTrees.Package;
+
+public class Checker implements Visitor<Object, Object>{
+	
+	private IdentificationTable idTable;
+	private ErrorReporter reporter;
+	
+	public Checker(ErrorReporter reporter){
+		this.reporter = reporter;
+		this.idTable = new IdentificationTable();
+		//establishStdEnvironment()?
+	}
+	
+	public void check(Package ast){
+		ast.visit(this, null);
+	}
+
+	@Override
+	public Object visitPackage(Package prog, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitClassDecl(ClassDecl cd, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitFieldDecl(FieldDecl fd, Object arg) {
+		// TODO 
+		
+		fd.type.visit(this, null);	// Assign to variable of datatype Type
+		
+		return null;
+	}
+
+	@Override
+	public Object visitMethodDecl(MethodDecl md, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitParameterDecl(ParameterDecl pd, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitVarDecl(VarDecl decl, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitBaseType(BaseType type, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitClassType(ClassType type, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitArrayType(ArrayType type, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitBlockStmt(BlockStmt stmt, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitVardeclStmt(VarDeclStmt stmt, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitAssignStmt(AssignStmt stmt, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitCallStmt(CallStmt stmt, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitIfStmt(IfStmt stmt, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitWhileStmt(WhileStmt stmt, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitUnaryExpr(UnaryExpr expr, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitBinaryExpr(BinaryExpr expr, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitRefExpr(RefExpr expr, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitCallExpr(CallExpr expr, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitLiteralExpr(LiteralExpr expr, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitNewObjectExpr(NewObjectExpr expr, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitNewArrayExpr(NewArrayExpr expr, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitQualifiedRef(QualifiedRef ref, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitIndexedRef(IndexedRef ref, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitIdRef(IdRef ref, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitThisRef(ThisRef ref, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitIdentifier(Identifier id, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitOperator(Operator op, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitIntLiteral(IntLiteral num, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitBooleanLiteral(BooleanLiteral bool, Object arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	// should implement Visitor in order to access the AST
 	
--- src/miniJava/ContextualAnalyzer/IdentificationTable.java
+++ src/miniJava/ContextualAnalyzer/IdentificationTable.java
@@ -1,51 +1,74 @@
 package miniJava.ContextualAnalyzer;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import miniJava.AbstractSyntaxTrees.Declaration;
 
 public class IdentificationTable {
-	// Scope follows the nested block structure
-	
-	// field for a hash map?
-	
+		
 	private int level;
+	private ArrayList<HashMap<String, Declaration>> listOfHashMaps;
 	
 	// constructor
 	public IdentificationTable(){
 		// Make an empty id table
-		
-		level = 0;
+		level = 1;	// outermost scope
+		listOfHashMaps = new ArrayList<HashMap<String, Declaration>>();
 	}
 	
 	// Call on defining occurrence of an id
-	public void enter(String id, Declaration attr){
-		// Add an entry to the identification table, associating identifier "id"
-		// with declaration "attr", which is a pointer to subtree in AST.
+	// Add an entry to the identification table, associating identifier "id"
+	// with declaration "attr", which is a pointer to subtree in AST.
+	public void enter(String id, Declaration attr){		
+		
+		// use for loop to iterate backwards through the listOfHashTables and
+		// look for duplicates in local or outer scopes
+		for(int i = level-1; i>=0;i--){
+			HashMap<String, Declaration> tempMap = listOfHashMaps.get(i);
+			if(tempMap.containsKey(id)){
+				// TODO there is a duplicate!!!
+			}
+		}
+		
+		// Add new entry
+		HashMap<String, Declaration> currScopeMap = listOfHashMaps.get(level-1);
+		currScopeMap.put(id, attr);
 	}
 	
 	// Call on applied occurrence of an id
+	// Return the declaration associated with identifier "id" in the
+	// identification table. If there are several entries for "id",
+	// return the declaration from the entry at the highest scope level.
+	// If there is no entry for "id", return null.
 	public Declaration retrieve(String id){
-		// Return the declaration associated with identifier "id" in the
-		// identification table. If there are several entries for "id",
-		// return the declaration from the entry at the highest scope level.
-		// If there is no entry for "id", return null.
-		
-		
-		
-		// TODO if entry for id not found, return an error
-		
-		// TODO Subject to change
+
+		// if decl found in local scope, return it; else go one scope back
+		for(int i = level-1; i>=0;i--){
+			HashMap<String, Declaration> tempMap = listOfHashMaps.get(i);
+			if(tempMap.containsKey(id)){
+				Declaration attr = tempMap.get(id);
+				return attr;
+			}
+		}
+
+		// TODO if entry for id not found, return an error or null?
 		return null;
 	}
 	
-	
+
+	// Add a new highest scope level to the identification table.
 	public void openScope(){
-		// Add a new highest scope level to the identification table.
+		HashMap<String, Declaration> newMap = new HashMap<String, Declaration>();
+		listOfHashMaps.add(newMap);
 		level++;
 	}
 	
+	
+	// Remove the highest scope level from the identification table,
+	// and all entries belonging to it.	
 	public void closeScope(){
-		// Remove the highest scope level from the identification table,
-		// and all entries belonging to it.
+		listOfHashMaps.remove(level);
 		level--;
 	}
 	
